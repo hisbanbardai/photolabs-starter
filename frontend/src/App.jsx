efa5610df3521d27a465ca8efb2caf6f360344c4
Hisban Shiraz <hisbanbardai@gmail.com>
2023-12-23 16:03:22 -0700

    Add child components (TopicList and FavBadge) to TopNavigation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,15 @@ import "./App.scss";
 import PhotoList from "components/PhotoList";
 import TopicListItem from "components/TopicListItem";
 import TopicList from "components/TopicList";
+import TopNavigation from "components/TopNavigationBar";
+import FavBadge from "components/FavBadge";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
+      <TopNavigation />
       <PhotoList />
-      <TopicList />
     </div>
   );
 };

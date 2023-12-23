eaa529192ace04f4d69a64a6f43435c17faacb26
Hisban Shiraz <hisbanbardai@gmail.com>
2023-12-23 16:37:18 -0700

    Remove sample data and add mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,31 +2,14 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const TopicList = () => {
-  const topics = sampleDataForTopicList.map((topic) => {
+  const renderTopics = topics.map((topic) => {
     return <TopicListItem key={topic.id} title={topic.title} />;
   });
 
-  return <div className="top-nav-bar__topic-list">{topics}</div>;
+  return <div className="top-nav-bar__topic-list">{renderTopics}</div>;
 };
 
 export default TopicList;

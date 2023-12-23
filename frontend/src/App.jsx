b9b4510ddbbb7f694009451557ccf1c676eb2b09
Hisban Shiraz <hisbanbardai@gmail.com>
2023-12-23 16:17:14 -0700

    Add HomeRoute component and move TopNavigation and PhotoList components to it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-
-import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
-import PhotoList from "components/PhotoList";
-import TopicListItem from "components/TopicListItem";
-import TopicList from "components/TopicList";
-import TopNavigation from "components/TopNavigationBar";
-import FavBadge from "components/FavBadge";
+import HomeRoute from "routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
